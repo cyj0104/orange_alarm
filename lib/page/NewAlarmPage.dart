9277cc3cab4407baf4c80fa17b92e4af66401b5c
CYJ <cyj0104@gmail.com>
2024-02-25 00:46:49 +0900

    요일 선택 추가

--- lib/page/NewAlarmPage.dart
+++ lib/page/NewAlarmPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class NewAlarmPage extends StatefulWidget {
   @override
@@ -21,18 +20,22 @@ class _NewAlarmPageState extends State<NewAlarmPage> {
     }
   }
 
-  bool is24HourFormat = false;
+  List<String> weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+  List<bool> isSelected = [false, false, false, false, false, false, false];
 
-  Future<void> checkSystemTimeFormat() async {
-    is24HourFormat = await MethodChannel('flutter.platform').invokeMethod('SystemChrome.is24HourFormat');
+  void toggleSelection(int index) {
+    setState(() {
+      isSelected[index] = !isSelected[index];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade700,
       appBar: AppBar(
         title: const Text('새 알람',style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.orange.shade900,
+        backgroundColor: Colors.orange.shade800,
         leading: IconButton(
           icon: const Icon(
             Icons.arrow_back,
@@ -46,7 +49,9 @@ class _NewAlarmPageState extends State<NewAlarmPage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          ///////////// 시간 선택
           Container(
+            padding: const EdgeInsets.symmetric(vertical: 50),
             alignment: Alignment.center,
             child: GestureDetector(
               onTap: () {
@@ -54,10 +59,43 @@ class _NewAlarmPageState extends State<NewAlarmPage> {
               },
               child: Text(
                 '${_selectedTime.period == DayPeriod.am ? 'AM' : 'PM'} ${_selectedTime.hourOfPeriod.toString().padLeft(2, '0')}:${_selectedTime.minute.toString().padLeft(2, '0')}',
-                style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+                style: const TextStyle(fontSize: 80, color: Colors.white, fontWeight: FontWeight.bold),
               ),
             ),
           ),
+
+          ///////////// 요일 선택
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: List.generate(
+                weekdays.length,
+                (index) => InkWell(
+                  onTap: () {
+                    toggleSelection(index);
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        child: Text(
+                          weekdays[index],
+                          style: TextStyle(
+                            fontSize: 30, color: isSelected[index] ? Colors.orange.shade800 : Colors.white,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ///////////// 알람음 선택
+          ///////////// 다시 울림
+          ///////////// 알람 끄기 미션
+          ///////////// 취소, 저장 버튼
         ],
       ),
     );
