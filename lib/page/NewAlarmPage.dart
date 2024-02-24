d5bb06cf7e2a60a540d8dc2673b95f073b9d5c3d
CYJ <cyj0104@gmail.com>
2024-02-24 23:33:53 +0900

    NewAlaremPage에 시계 선택 추가

--- lib/page/NewAlarmPage.dart
+++ lib/page/NewAlarmPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewAlarmPage extends StatefulWidget {
   @override
@@ -6,8 +7,25 @@ class NewAlarmPage extends StatefulWidget {
 }
 
 class _NewAlarmPageState extends State<NewAlarmPage> {
+  TimeOfDay _selectedTime = TimeOfDay.now();
 
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+    );
+    if (picked != null && picked != _selectedTime) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
+  }
 
+  bool is24HourFormat = false;
+
+  Future<void> checkSystemTimeFormat() async {
+    is24HourFormat = await MethodChannel('flutter.platform').invokeMethod('SystemChrome.is24HourFormat');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,8 +43,22 @@ class _NewAlarmPageState extends State<NewAlarmPage> {
           },
         ),
       ),
-      body: Center(
-        child: Text('Add Alarm Form'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: () {
+                _selectTime(context);
+              },
+              child: Text(
+                '${_selectedTime.period == DayPeriod.am ? 'AM' : 'PM'} ${_selectedTime.hourOfPeriod.toString().padLeft(2, '0')}:${_selectedTime.minute.toString().padLeft(2, '0')}',
+                style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
