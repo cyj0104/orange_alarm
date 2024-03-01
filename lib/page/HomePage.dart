ace9f816c493d23780215ffdbe00f6cd0a86fcf6
CYJ <cyj0104@gmail.com>
2024-03-02 08:21:56 +0900

    알람 추가 동작 UI 구현

--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,25 +1,70 @@
 import "package:flutter/material.dart";
 import "package:orange_alarm/page/NewAlarmPage.dart";
+import "../widget/ContainerForAlarmItem.dart";
+
 
 
 class Homepage extends StatefulWidget {
-  const Homepage({super.key});
-
   @override
   _HomepageState createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
+  List<Widget> alarmList = [];
+
+
+  void addAlarm() {
+    int dateTime = DateTime.now().millisecondsSinceEpoch;
+    late Key key = ValueKey(dateTime);
+
+    setState(() {
+      alarmList.add(
+        ContainerForAlarmItem(
+          key: key,
+          onRemove: removeAlarm,
+        ),
+      );
+    });
+  }
+
+  void removeAlarm(Key key) {
+    setState(() {
+      alarmList.removeWhere((container) => (container.key) == key);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade800,
+      backgroundColor: Colors.blueGrey.shade800,
       appBar: AppBar(
-        title: const Text('알람', style: TextStyle(color: Colors.white)),
+        title: Text('알람', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.orange.shade900,
       ),
-      body: const Center(
-        child: Text('알람 목록', style: TextStyle(color: Colors.white)),
+      body: ListView(
+        //mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              addAlarm();
+            },
+            child: Text('Add Container'),
+          ),
+          ...alarmList,
+          /////////////// 삭제할 코드 시작
+          // GestureDetector(
+          //   onTap: () {
+          //     Navigator.push(
+          //         context,
+          //         MaterialPageRoute(builder: (context) => SnoozeAndTurnOffAlarmPage()),
+          //     );
+          //   },
+          // )
+          Padding(
+              padding: EdgeInsets.only(bottom: 90)
+          ),
+        ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -29,9 +74,12 @@ class _HomepageState extends State<Homepage> {
           );
         },
         backgroundColor: Colors.orange.shade900,
-        shape: const CircleBorder(),
-        child: const Icon(Icons.add, color: Colors.white),
+        shape: CircleBorder(),
+        child: Icon(Icons.add, color: Colors.white),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
     );
   }
 }
+
+
---
+++ lib/widget/ContainerForAlarmItem.dart
@@ -0,0 +1,69 @@
+import "package:flutter/material.dart";
+
+class ContainerForAlarmItem extends StatefulWidget {
+  @override
+  final Key key;
+  final Function(Key) onRemove;
+
+  const ContainerForAlarmItem({
+    required this.key,
+    required this.onRemove,
+  }): super(key: key);
+
+  @override
+  _ContainerForAlarmItemState createState() =>  _ContainerForAlarmItemState();
+}
+
+class _ContainerForAlarmItemState extends State<ContainerForAlarmItem> {
+  bool switchButton = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          key: widget.key,
+          margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.blueGrey.shade200,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('AM 10:00', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600)),
+                    Text('일, 월, 화, 수, 목, 금, 토', style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15, top: 10, bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Switch(
+                      value: switchButton,
+                      activeColor: Colors.orange.shade900,
+                      onChanged: (value) {
+                        setState(() {
+                          switchButton = value;
+                        });
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        widget.onRemove(widget.key);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+}
